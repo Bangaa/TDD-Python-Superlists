d8bad947d0563ff35b32c5243fc586ab5588a892
Ian Mejias <idmconejeros@gmail.com>
2017-08-13 18:01:37 -0300

    Se agrega la nueva URL 'lists/new' para agregar nuevos elementos.

--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
+    return render(request, 'home.html')
+
+def view_list(request):
+    return render(request, 'list.html', {'items': Item.objects.all()})
+
+def new_list(request):
     if request.method == "POST":
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
 
-    return render(request, 'home.html')
-
-def view_list(request):
-    return render(request, 'list.html', {
-            'items': Item.objects.all()
-        })

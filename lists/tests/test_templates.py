93317df876d8dfad085ddc136ee1e27670df2a94
Ian Mejias <idmconejeros@gmail.com>
2017-08-17 19:09:55 -0300

    REFACTOR: Se reorganizan los tests de las distintas 'apps'

--- lists/tests/test_templates.py
+++ lists/tests/test_templates.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Tests de las vistas de la app 'List'
+
 from django.test import TestCase
-
-from django.urls import resolve
-from django.http import HttpRequest
 from lists.views import home_page   # Funcion que retorna la URL de home
-
-# TEST DE LAS VISTAS
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -81,39 +82,4 @@ class NewListTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
-
-# TEST DE LOS MODELOS
-
-from lists.models import Item, List
-
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(list_, saved_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
 

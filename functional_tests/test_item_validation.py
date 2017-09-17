7648b18a45970f0cc3ce97ee60cfb3b52e2fc45f
Ian Mejias <idmconejeros@gmail.com>
2017-09-17 02:17:05 -0300

    Se usa 'ItemForm' en todas las vistas.
    Todos los tests funcionando.

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -3,6 +3,7 @@
 
 from unittest import skip
 from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
 
@@ -10,31 +11,38 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
-        self.add_todo_element("")
+        self.add_todo_element("", 10)
 
-        # The home page refreshes, and there is an error message saying
-        # that list items cannot be blank
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            ItemForm.Meta.error_messages['text']['required']
-            )
-        )
+        # The browser intercepts the request, and does not load the
+        # list page
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
 
-        # She tries again with some text for the item, which now works
-        self.add_todo_element("Comprar pan")
-        self._assertRowInTable("Comprar pan")
+        # She starts typing some text for the new item and the error disappears
+        self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+
+        # And she can submit it successfully
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self._assertRowInTable('Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
-        self.add_todo_element("")
+        self.add_todo_element("", 10)
 
-        # She receives a similar warning on the list page
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            ItemForm.Meta.error_messages['text']['required']
-            )
-        )
+        # Again, the browser will not comply
+        self._assertRowInTable('Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
 
         # And she can correct it by filling some text in
-        self.add_todo_element("Hacerme un sandwich")
-        self._assertRowInTable("Comprar pan")
-        self._assertRowInTable("Hacerme un sandwich")
+        self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self._assertRowInTable('Buy milk')
+        self._assertRowInTable('Make tea')

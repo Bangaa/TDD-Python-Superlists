cf7fa135b55be5cc2594df71bc1a8e566766f635
Ian Mejias <idmconejeros@gmail.com>
2017-10-17 17:15:15 -0300

    FT para el login via email.

---
+++ functional_tests/test_login.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+from django.core import mail
+from selenium.webdriver.common.keys import Keys
+import re
+
+from .base import FunctionalTest
+
+
+
+class LoginTest(FunctionalTest):
+
+    def setUp(self):
+        super().setUp()
+        self.TEST_EMAIL = 'edith@example.com'
+        self.SUBJECT = 'Your login link for Superlists'
+
+    def test_can_get_email_link_to_log_in(self):
+        # Edith goes to the awesome superlists site
+        # and notices a "Log in" section in the navbar for the first time
+        # It's telling her to enter her email address, so she does
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_name('email').send_keys(self.TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+
+        # A message appears telling her an email has been sent
+        self.wait_for(lambda: self.assertIn(
+            'Check your email',
+            self.browser.find_element_by_tag_name('body').text
+        ))
+
+        # She checks her email and finds a message
+        email = mail.outbox[0]
+        self.assertIn(self.TEST_EMAIL, email.to)
+        self.assertEqual(email.subject, self.SUBJECT)
+
+        # It has a url link in it
+        self.assertIn('Use this link to log in', email.body)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail('Could not find url in email body:\n%s' % email.body)
+        url = url_search.group(0)
+        self.assertIn(self.live_server_url, url)
+
+        # she clicks it
+        self.browser.get(url)
+
+        # she is logged in!
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(self.TEST_EMAIL, navbar.text)

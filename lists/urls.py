ac91eb9882acd47860c1da8227f71a1ea6b3beab
Ian Mejias <idmconejeros@gmail.com>
2018-04-06 17:45:39 -0300

    Preparaciones basicas para la vista de listas del usuario
     - Se agrega link a 'base.html'
     - Se crea un template para las listas del usuario
     - Se crea una URL y su controlador correspondiente

--- lists/urls.py
+++ lists/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 
-import lists.views
+from lists import views
 
 urlpatterns = [
-    url(r'^new$', lists.views.new_list, name='new_list'),
-    url(r'^(\d+)/$', lists.views.view_list, name='view_list'),
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
